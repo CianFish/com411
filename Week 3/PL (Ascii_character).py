e869cb02ac9eeb4aa605070dfdaff914e19bb787
Cian Fish <5fishc83@solent.ac.uk>
2021-10-14 16:51:33 +0100

    Week 3 labs

--- Week 3/PL (Ascii_character).py
+++ Week 3/PL (Ascii_character).py
@@ -2,7 +2,7 @@ def ASCII_char():
     print("Program Started!")
     print("Please enter an ASCII code:")
     code = int(input())
-    character = chr(code)
+    character = chr(code) #chr finds the ASCII charater for the number
 
     if code >=32 and code <=126:
         print(f"The character represented by the ASCII code {code} is: {character}")
