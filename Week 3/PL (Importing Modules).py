1337dca1fb63d128ff0cd8c1ab8b28603b230890
Cian <5fishc83@solent.ac.uk>
2021-10-19 09:51:34 +0100

    Completed all but main

--- Week 3/PL (Importing Modules).py
+++ Week 3/PL (Importing Modules).py
@@ -1,18 +1,39 @@
+import random
 import random as rnd
-from random import gauss
 
-print("Please enter the minimum value:")
-min = int(input())
+def asking_user(min, max,):
+    print("Please enter the minimum value:")
+    min = int(input())
 
-print("\nPlease enter the maximum value: ")
-max = int(input())
+    print("\nPlease enter the maximum value: ")
+    max = int(input())
 
-print(f"\nI am thinking of a number between {min} and {max}. Can you guess what it is?")
-ans = int(input())
-computer_ans = gauss(min, max)
+    repeating_question(min, max)
 
 
+def repeating_question(min, max):
+    computer_ans = random.randrange(min, max)
 
+    print(f"I am thinking of a number between {min} and {max}.\nCan you guess what is is?")
+
+    correct_ans = False
+
+    while not correct_ans:
+        print("Please enter a number:")
+        ans = int(input())
+
+        if ans == computer_ans:
+          print("Congratulations! You guessed my number!")
+          correct_ans = True
+        elif ans >= computer_ans:
+            print("Your guess is to high.\nTry again:")
+        elif ans <= computer_ans:
+            print("Your guess is to low.\nTry again:")
+
+    print("Game over!")
+
+
+asking_user(min, max)
 
 #for _ in range (0, max, 1):
 #    if ans == computer_ans:
