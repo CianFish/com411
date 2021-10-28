d5931706f2dc7a8ed435be66aae741120bc46cd2
Cian Fish <5fishc83@solent.ac.uk>
2021-10-28 16:50:38 +0100

    Week 5 labs

---
+++ Week 5/PL07 (Nested Tuples).py
@@ -0,0 +1,13 @@
+def steps():
+    likelihoods = (("step 1", 50),("step 2", 38),("step 3", 27),("step 4", 99),("step 5", 4))
+    return likelihoods
+
+def run():
+    step = steps()
+    good_steps = []
+    bad_steps = []
+    for list in range(len(step)):
+        if list => 50:
+            good_steps.append()
+        else:
+            bad_steps.append()
